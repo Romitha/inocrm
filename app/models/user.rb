d8d4a3b7c3d273e00a728c13150a12d16a6b907a
umesh m <umeshblader@gmail.com>
2014-10-06 13:27:54 +0530

    re #4 - user model authentication is update to response only authentication purpose

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,:confirmable
+         :recoverable, :rememberable, :trackable, :validatable#,:confirmable
 
-  def only_if_unconfirmed
-    pending_any_confirmation {yield}
-  end
+  # def only_if_unconfirmed
+  #   pending_any_confirmation {yield}
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922043654) do
+ActiveRecord::Schema.define(version: 20141006045326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organizations", force: true do |t|
+    t.string   "name"
+    t.string   "short_name"
+    t.string   "website"
+    t.string   "vat_number"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
