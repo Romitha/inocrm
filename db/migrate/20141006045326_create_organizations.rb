1698271a7c78347de1fa9ee28b016d99c18daebf
umesh m <umeshblader@gmail.com>
2014-10-06 13:35:04 +0530

    re #12 - Organization level management resource is initiated.

---
+++ db/migrate/20141006045326_create_organizations.rb
@@ -0,0 +1,12 @@
+class CreateOrganizations < ActiveRecord::Migration
+  def change
+    create_table :organizations do |t|
+    	t.string :name
+    	t.string :short_name
+    	t.string :website
+    	t.string :vat_number
+    	t.string :type
+      t.timestamps
+    end
+  end
+end
