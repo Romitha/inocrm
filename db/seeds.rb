74428a55456fcf64a2831e00ea171d9dadf181f3
umesh m <umeshblader@gmail.com>
2014-10-07 10:00:23 +0530

    re #4 - user management system related roles and permission feature is initiated.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create email: "admin@inovacrm.com", password: "123456789" unless User.find_by_email("admin@inovacrm.com")
+Organization.find_or_create_by name: "VS Information Systems", website: "http://www.vsis.com", vat_number: "34358-90", refers: "VSIS"
