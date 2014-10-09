e7d11737e6ef16c26d843d294b01b206fb0f2bbd
sharmilan <sharmilan.somasundaram@inovaitsys.com>
2014-10-09 15:26:35 +0530

    re #17 - activity log model and table structure are created

---
+++ db/migrate/20141009094719_create_activity_logs.rb
@@ -0,0 +1,15 @@
+class CreateActivityLogs < ActiveRecord::Migration
+  def change
+    create_table :activity_logs do |t|
+      t.string :user
+      t.string :role
+      t.string :browser
+      t.string :os
+      t.string :ip_address
+      t.string :event
+      t.string :reference_url
+      t.string :to_user
+      t.timestamps
+    end
+  end
+end
