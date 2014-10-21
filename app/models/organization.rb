602f4a935a75f7fd0a192ff007b6691458071478
sharmilan <sharmilan.somasundaram@inovaitsys.com>
2014-10-21 16:45:26 +0530

    re #39 - contact_number model is initiated.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,4 +5,9 @@ class Organization < ActiveRecord::Base
 
 	accepts_nested_attributes_for :addresses, allow_destroy: true
 
+	has_many :contact_numbers
+
+	accepts_nested_attributes_for :contact_numbers, allow_destroy: true
+
+	validates_format_of :web_site, :with => URI::regexp(%w(http https))
 end
