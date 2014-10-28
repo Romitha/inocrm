31a6d090dd30e817348ef1be5b66b7031478b840
Shirhan Ousmand <shirhan@inovaitsys.com>
2014-10-28 15:40:00 +0530

    re #24 - Added polymorphic constraints for Organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,12 +1,10 @@
 class Organization < ActiveRecord::Base
-	mount_uploader :logo, LogoUploader
+    mount_uploader :logo, LogoUploader
 
-	has_many :addresses
-
-	accepts_nested_attributes_for :addresses, allow_destroy: true
+    has_many :addresses, as: :addressable
+    accepts_nested_attributes_for :addresses, allow_destroy: true
 
 	has_many :contact_numbers
-
 	accepts_nested_attributes_for :contact_numbers, allow_destroy: true
 
 	validates_format_of :web_site, :with => URI::regexp(%w(http https))
