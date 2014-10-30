aa35fc06b7e424c5434f4a60e3de7706474d4c83
umesh m <umesh.m@inovaitsys.com>
2014-10-30 18:14:04 +0530

    re #13 - invalid migrations are removed for now, and it will be used in future phase

--- db/migrate/20141014102233_master_industry_types.rb
+++
@@ -1,9 +0,0 @@
-class MasterIndustryTypes < ActiveRecord::Migration
-  def change
-  	create_table (:mst_industry_types) do |t|
-		t.string 		:name,      limit: 200
-		t.string 		:code,      limit: 3
-		t.boolean 		:deleted
-	end
-  end
-end
--- db/migrate/20141014104626_create_master_designations.rb
+++
@@ -1,8 +0,0 @@
-class CreateMasterDesignations < ActiveRecord::Migration
-  def change
-    create_table :mst_designations do |t|
-		t.string	:name,	limit:200
-		t.boolean	:deleted
-    end
-  end
-end
