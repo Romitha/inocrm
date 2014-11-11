91c986908b8cd30242b4ac2b1431af5609ca74b5
Shirhan Ousmand <shirhan@inovaitsys.com>
2014-11-11 17:01:42 +0530

    re #36 Added constraint "Organization has many users"

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,16 +4,18 @@ class Organization < ActiveRecord::Base
   has_many :addresses, as: :addressable
   accepts_nested_attributes_for :addresses, allow_destroy: true
 
-	has_many :contact_numbers, as: :c_numberable
-	accepts_nested_attributes_for :contact_numbers, allow_destroy: true
+  has_many :contact_numbers, as: :c_numberable
+  accepts_nested_attributes_for :contact_numbers, allow_destroy: true
 
   has_many :designations
 
-	validates_format_of :web_site, :with => URI::regexp(%w(http https))
+  #2014_11_11
+  has_many :users
 
 	TYPES = %w(Supplier Customer)
 
 	scope :suppliers, -> {where(category: "Supplier")}
 	scope :customers, -> {where(category: "Customer")}
 
+  validates_format_of :web_site, :with => URI::regexp(%w(http https))
 end
