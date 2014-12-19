e50c588c36eb28f8d31c9b03207aabfc3864213d
umesh m <umesh.m@inovaitsys.com>
2014-12-19 14:12:56 +0530

    upload configuration is setup according to different environments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.sftp_host = "192.168.1.193"
   config.sftp_user = "dev"
-  config.sftp_folder = "/home/dev/inovacrm"
-  config.sftp_url = "http://192.168.1.193/assets/inovacrm"
+  config.sftp_folder = "/home/dev/inovacrm/#{Rails.env}"
+  config.sftp_url = "http://192.168.1.193/assets/inovacrm/#{Rails.env}"
   config.sftp_options = {
     :password => "dev123",
     :port     => 22
