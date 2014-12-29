02392f06b952132357d5ece0f7c6d540f289c986
sharmilan <sharmilan.somasundaram@inovaitsys.com>
2014-12-29 17:28:00 +0530

    primary and non primary addresses and contact numbers are seperated. organization upload image dynamic presentation is initiated.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,14 @@
 class Address < ActiveRecord::Base
-    #Users and Organizations have multiple addresses
-    belongs_to :addressable, polymorphic: true
+  #Users and Organizations have multiple addresses
+  belongs_to :addressable, polymorphic: true
 
-    TYPES = %w(Billing Shipping Support Office Home)
+  TYPES = %w(Billing Shipping Support Office Home)
 
-    validates :address, presence: true
-    validates :category, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
 
-    validates_uniqueness_of :primary, conditions: -> { where(primary: true)}, scope: [:addressable_id, :addressable_type]
+  validates_uniqueness_of :primary, conditions: -> { where(primary: true)}, scope: [:addressable_id, :addressable_type]
 
+  scope :primary_address, -> {where(primary: true)}
+  scope :non_primary_address, -> {where(primary: false)}
 end
