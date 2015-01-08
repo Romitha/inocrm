4883194c42bcd41ca861320eff462c8b47cd58cd
umesh m <umesh.m@inovaitsys.com>
2015-01-08 11:37:38 +0530

    file uploading server changed.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,9 @@
+stfp_server = "192.168.1.146"
 CarrierWave.configure do |config|
-  config.sftp_host = "192.168.1.193"
+  config.sftp_host = stfp_server
   config.sftp_user = "dev"
   config.sftp_folder = "/home/dev/inovacrm/#{Rails.env}"
-  config.sftp_url = "http://192.168.1.193/assets/inovacrm/#{Rails.env}"
+  config.sftp_url = "http://#{stfp_server}/assets/inovacrm/#{Rails.env}"
   config.sftp_options = {
     :password => "dev123",
     :port     => 22
