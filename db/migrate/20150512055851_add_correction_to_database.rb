abf5184aa902dd6b123f58aae5f6d3a108ee2edc
ranuri <ranurilakma@gmail.com>
2015-05-27 18:05:36 +0530

    Crated TO DO List UI with todo resources.

--- db/migrate/20150512055851_add_correction_to_database.rb
+++
@@ -1,9 +0,0 @@
-class AddCorrectionToDatabase < ActiveRecord::Migration
-  def change
-  	remove_column :dyna_columns, :id
-  	add_column :dyna_columns, :id, "INT UNSIGNED"
-
-  	remove_column :dyna_columns, :resourceable_id
-  	add_column :dyna_columns, :resourceable_id, "INT UNSIGNED"
-  end
-end
