1bf3ee214e72ead3065ab62d16b09161860f1b79
Ranuri <ranuri.lakma@inovaitsys.com>
2015-06-05 16:07:58 +0530

    view ticket front-end is updated.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -16,6 +16,7 @@ class TicketsController < ApplicationController
   def show
     Warranty
     ContactNumber
+    QAndA
     # @tickets = @ticket.joint_tickets
     # Rails.cache.fetch([:histories, session[:product_id]], Kaminari.paginate_array(@product.tickets)) do
       
