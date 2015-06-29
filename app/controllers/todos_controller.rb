4b24bf8b3832ed143f2f2ccb41c4af09f55868b9
Umesh M <umesh.m@inovaitsys.com>
2015-06-30 00:36:50 +0530

    latest updated.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,24 +3,28 @@ class TodosController < ApplicationController
     Rails.cache.delete([:workflow_mapping_role, "assign_ticket"])
     Rails.cache.delete([:workflow_mapping_user, "assign_ticket"])
 
-    @potential_owner = current_user.roles.find_by_id(current_user.current_user_role_id).bpm_module_roles.first.code
+    @potential_owner = current_user.roles.find_by_id(current_user.current_user_role_id).bpm_module_roles.first.try :code
     @todo_list_for_role = []
     @workflow_mapping_for_role = []
 
     @todo_list_for_user = []
     @workflow_mapping_for_user = []
 
-    ["InProgress", "Reserved"].each do |status|
-      @todo_list_for_role << view_context.send_request_process_data(task_list: true, status: status, potential_owner: @potential_owner, query: {})
+    if @potential_owner
+      ["InProgress", "Reserved"].each do |status|
+        @todo_list_for_role << view_context.send_request_process_data(task_list: true, status: status, potential_owner: @potential_owner, query: {})
+      end
+
+      @task_content_for_role = @todo_list_for_role.map { |list| list[:content] and list[:content]["task_summary"] }.compact.flatten
+
+      @task_content_for_role.each do |task_content|
+        @workflow_mapping_for_role << {workflow_mapping: Rails.cache.fetch([:workflow_mapping_role, task_content["name"]]){WorkflowMapping.where(task_name: task_content["name"], process_name: task_content["process_id"]).first}, workflow_header: Rails.cache.fetch([:workflow_header, task_content["process_instance_id"]]){WorkflowHeaderTitle.find_by_process_id(task_content["process_instance_id"])}, task_content: task_content}
+      end
+      puts @workflow_mapping_for_role
+      @formatted_workflow_mapping_for_role = @workflow_mapping_for_role.map{|w| {process_name: w[:workflow_mapping].process_name, task_name: w[:workflow_mapping].task_name, url: w[:workflow_mapping].url, first_header_title: w[:workflow_mapping].first_header_title, second_header_title_name: w[:workflow_mapping].second_header_title_name, input_variables: w[:workflow_mapping].input_variables, second_header_title: (w[:workflow_header].send(w[:workflow_mapping].second_header_title_name.to_sym) if w[:workflow_header]), task_content: w[:task_content]}}
+      
     end
 
-    @task_content_for_role = @todo_list_for_role.map { |list| list[:content] and list[:content]["task_summary"] }.compact.flatten
-
-    @task_content_for_role.each do |task_content|
-      @workflow_mapping_for_role << {workflow_mapping: Rails.cache.fetch([:workflow_mapping_role, task_content["name"]]){WorkflowMapping.where(task_name: task_content["name"], process_name: task_content["process_id"]).first}, workflow_header: Rails.cache.fetch([:workflow_header, task_content["process_instance_id"]]){WorkflowHeaderTitle.find_by_process_id(task_content["process_instance_id"])}, task_content: task_content}
-    end
-
-    @formatted_workflow_mapping_for_role = @workflow_mapping_for_role.map{|w| {process_name: w[:workflow_mapping].process_name, task_name: w[:workflow_mapping].task_name, url: w[:workflow_mapping].url, first_header_title: w[:workflow_mapping].first_header_title, second_header_title_name: w[:workflow_mapping].second_header_title_name, input_variables: w[:workflow_mapping].input_variables, second_header_title: (w[:workflow_header].send(w[:workflow_mapping].second_header_title_name.to_sym) if w[:workflow_header]), task_content: w[:task_content]}}
 
     ["InProgress", "Reserved"].each do |status|
       @todo_list_for_user << view_context.send_request_process_data(task_list: true, status: status, potential_owner: current_user.id, query: {})
