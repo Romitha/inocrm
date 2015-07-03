18ad17c106f6ccec4bd1a08c5cf409056241543f
sharmilan <sharmilan.somasundaram@inovaitsys.com>
2015-07-03 13:04:18 +0530

    estimation and initial values are updated.

--- db/migrate/20150428062024_insert_initial_values.rb
+++ db/migrate/20150428062024_insert_initial_values.rb
@@ -238,35 +238,35 @@ class InsertInitialValues < ActiveRecord::Migration
     # end
 
     [
-      ["SPPT", "add_ticket", "/ticket/new", "SPT_SC_1", "Support - Edit Ticket", "h1", "map_ticket_id,map_supp_hd_user",nil],
-      ["SPPT", "assign_ticket", "/ticket/assign_ticket", "SPT_SC_2", "Support - Assign Ticket", "h1", "map_ticket_id", nil],
-      ["SPPT", "resolution", "", "SPT_SC_3", "Support - Resolution", "h1", "map_ticket_id,map_supp_engr_user", nil],
-      ["SPPT", "pop_approval", "", "SPT_SC_5", "Support - POP Approval", "h1", "map_ticket_id", nil],
-      ["SPPT", "edit_serial_no", "", "SPT_SC_6", "Support - Edit Serial No", "h1", "map_ticket_id", nil],
-      ["SPPT", "job_estimate", "", "SPT_SC_7", "Support - Job Estimation", "h1", "map_ticket_id", nil],
-      ["SPPT", "mark_delivered_colleted", "", "SPT_SC_8", "Support - Units To Be Delivered or Colleced for External Repaire", nil, "map_ticket_id,map_deliver_unit_id", nil],
-      ["SPPT_MFR_PART_REQUEST", "order_part", "", "SPT_SC_9", "Support - Order Part (Manufacture)", "h3", "map_ticket_id,map_request_spare_part_id,map_supp_engr_user", nil],
-      ["SPPT_MFR_PART_REQUEST", "receive_Issue_part", "", "SPT_SC_10", "Support - Receive or Issue Part (Manufacture)", "h3", "map_ticket_id,map_request_spare_part_id,map_supp_engr_user", nil],
-      ["SPPT_MFR_PART_RETURN", "return_manufacture_part", "", "SPT_SC_11", "Support - Return Part (Manufacture)", "h3", "map_ticket_id,map_request_spare_part_id,map_supp_engr_user", nil],
-      ["SPPT", "extend_warranty", "", "SPT_SC_12", "Support - Extend Warranty", "h1", "map_ticket_id", nil],
-      ["SPPT", "approve_close_ticket", "", "SPT_SC_13", "Support - Ticket Close Approval", "h1", "map_ticket_id,map_supp_engr_user", nil],
-      ["SPPT", "customer_feedback", "", "SPT_SC_14", "Support - Customer Feedback", "h1", "map_ticket_id,map_supp_engr_user", nil],
-      ["SPPT_PART_ESTIMATE", "part_estimate", "", "SPT_SC_15", "Support - Part Estimation (Store)", "h2", "map_ticket_id,map_part_estimation_id,map_supp_engr_user", nil],
-      ["SPPT_STORE_PART_REQUEST", "approve_store_part", "", "SPT_SC_16", "Support - Part Approval (Store)", "h2", "map_ticket_id,map_request_spare_part_id,map_request_onloan_spare_part_id,map_onloan_request,map_supp_engr_user", nil],
-      ["SPPT_STORE_PART_REQUEST", "issue_store_part", "", "SPT_SC_17", "Support - Issue Part (Store)", "h2", "map_ticket_id,map_request_spare_part_id,map_request_onloan_spare_part_id,map_onloan_request,map_supp_engr_user", nil],
-      ["SPPT_STORE_PART_RETURN", "return_store_part", "", "SPT_SC_18", "Support - Return Part (Store)", "h2", "map_ticket_id,map_request_spare_part_id,map_request_onloan_spare_part_id,map_onloan_request", nil],
-      ["SPPT", "approve_job_estimate", "", "SPT_SC_19", "Support - Job Estimation Approval", "h1", "map_ticket_id", nil],
-      ["SPPT_PART_ESTIMATE", "approve_part_estimate", "", "SPT_SC_20", "Support - Part Estimation Approval (Store)", "h2", "map_ticket_id,map_part_estimation_id,map_supp_engr_user", nil],
-      ["SPPT_MFR_PART_RETURN", "bundle_return_part", "", "SPT_SC_21", "Support - Return Parts To Be Bundled", nil, "map_ticket_id,map_request_spare_part_id", nil],
-      ["SPPT_MFR_PART_RETURN", "bundle_deliver", "", "SPT_SC_22", "Support - Return Parts Bundles To Be Delivered", nil, "map_bundle_id", nil],
-      ["SPPT_MFR_PART_REQUEST", "collect_part", "", "SPT_SC_23", "Support - Manufacture parts to be collected", nil, "map_ticket_id,map_request_spare_part_id", nil],
-      ["SPPT", "issue_customer_terminate", "", "SPT_SC_24", "Support - Terminated Job Return To Custormer", "h1", "map_ticket_id", nil],
+      ["SPPT", "add_ticket", "/tickets/edit-ticket", "SPT_SC_1", "Support - Edit Ticket", "h1", "ticket_id,supp_hd_user",nil],
+      ["SPPT", "assign_ticket", "/tickets/assign-ticket", "SPT_SC_2", "Support - Assign Ticket", "h1", "ticket_id", nil],
+      ["SPPT", "resolution", "/tickets/resolution", "SPT_SC_3", "Support - Resolution", "h1", "ticket_id,supp_engr_user", nil],
+      ["SPPT", "pop_approval", "/tickets/pop-approval", "SPT_SC_5", "Support - POP Approval", "h1", "ticket_id", nil],
+      ["SPPT", "edit_serial_no", "", "SPT_SC_6", "Support - Edit Serial No", "h1", "ticket_id", nil],
+      ["SPPT", "job_estimate", "", "SPT_SC_7", "Support - Job Estimation", "h1", "ticket_id", nil],
+      ["SPPT", "mark_delivered_colleted", "", "SPT_SC_8", "Support - Units To Be Delivered or Colleced for External Repaire", nil, "ticket_id,deliver_unit_id", nil],
+      ["SPPT_MFR_PART_REQUEST", "order_part", "", "SPT_SC_9", "Support - Order Part (Manufacture)", "h3", "ticket_id,request_spare_part_id,supp_engr_user", nil],
+      ["SPPT_MFR_PART_REQUEST", "receive_Issue_part", "", "SPT_SC_10", "Support - Receive or Issue Part (Manufacture)", "h3", "ticket_id,request_spare_part_id,supp_engr_user", nil],
+      ["SPPT_MFR_PART_RETURN", "return_manufacture_part", "", "SPT_SC_11", "Support - Return Part (Manufacture)", "h3", "ticket_id,request_spare_part_id,supp_engr_user", nil],
+      ["SPPT", "extend_warranty", "", "SPT_SC_12", "Support - Extend Warranty", "h1", "ticket_id", nil],
+      ["SPPT", "approve_close_ticket", "", "SPT_SC_13", "Support - Ticket Close Approval", "h1", "ticket_id,supp_engr_user", nil],
+      ["SPPT", "customer_feedback", "", "SPT_SC_14", "Support - Customer Feedback", "h1", "ticket_id,supp_engr_user", nil],
+      ["SPPT_PART_ESTIMATE", "part_estimate", "", "SPT_SC_15", "Support - Part Estimation (Store)", "h2", "ticket_id,part_estimation_id,supp_engr_user", nil],
+      ["SPPT_STORE_PART_REQUEST", "approve_store_part", "", "SPT_SC_16", "Support - Part Approval (Store)", "h2", "ticket_id,request_spare_part_id,request_onloan_spare_part_id,onloan_request,supp_engr_user", nil],
+      ["SPPT_STORE_PART_REQUEST", "issue_store_part", "", "SPT_SC_17", "Support - Issue Part (Store)", "h2", "ticket_id,request_spare_part_id,request_onloan_spare_part_id,onloan_request,supp_engr_user", nil],
+      ["SPPT_STORE_PART_RETURN", "return_store_part", "", "SPT_SC_18", "Support - Return Part (Store)", "h2", "ticket_id,request_spare_part_id,request_onloan_spare_part_id,onloan_request", nil],
+      ["SPPT", "approve_job_estimate", "", "SPT_SC_19", "Support - Job Estimation Approval", "h1", "ticket_id", nil],
+      ["SPPT_PART_ESTIMATE", "approve_part_estimate", "", "SPT_SC_20", "Support - Part Estimation Approval (Store)", "h2", "ticket_id,part_estimation_id,supp_engr_user", nil],
+      ["SPPT_MFR_PART_RETURN", "bundle_return_part", "", "SPT_SC_21", "Support - Return Parts To Be Bundled", nil, "ticket_id,request_spare_part_id", nil],
+      ["SPPT_MFR_PART_RETURN", "bundle_deliver", "", "SPT_SC_22", "Support - Return Parts Bundles To Be Delivered", nil, "bundle_id", nil],
+      ["SPPT_MFR_PART_REQUEST", "collect_part", "", "SPT_SC_23", "Support - Manufacture parts to be collected", nil, "ticket_id,request_spare_part_id", nil],
+      ["SPPT", "issue_customer_terminate", "", "SPT_SC_24", "Support - Terminated Job Return To Custormer", "h1", "ticket_id", nil],
       ["SPPT", "customer_advance_payment", "", "SPT_SC_27", "", nil, nil, nil],
-      ["SPPT", "quality_control", "", "SPT_SC_28", "Support - Quality Control", "h1", "map_ticket_id,map_supp_engr_user", nil],
-      ["SPPT", "invoice_advance_payment", "", "SPT_SC_29", "Support - Advance Payment Invoice", "h1", "map_ticket_id,map_advance_payment_estimation_id", nil],
-      ["SPPT_MFR_PART_RETURN", "close_event", "", "SPT_SC_30", "Support - Close Event", "h1", "map_ticket_id,map_request_spare_part_id", nil],
-      ["SPPT", "approve_foc", "", "SPT_SC_31", "Support - FOC Approval", "h1", "map_ticket_id", nil],
-      ["SPPT", "final_job_estimate", "", "SPT_SC_33", "Support - Final Job Estimation", "h1", "map_ticket_id", nil]
+      ["SPPT", "quality_control", "", "SPT_SC_28", "Support - Quality Control", "h1", "ticket_id,supp_engr_user", nil],
+      ["SPPT", "invoice_advance_payment", "", "SPT_SC_29", "Support - Advance Payment Invoice", "h1", "ticket_id,advance_payment_estimation_id", nil],
+      ["SPPT_MFR_PART_RETURN", "close_event", "", "SPT_SC_30", "Support - Close Event", "h1", "ticket_id,request_spare_part_id", nil],
+      ["SPPT", "approve_foc", "", "SPT_SC_31", "Support - FOC Approval", "h1", "ticket_id", nil],
+      ["SPPT", "final_job_estimate", "", "SPT_SC_33", "Support - Final Job Estimation", "h1", "ticket_id", nil]
     ].each do |value|
       execute("insert into workflow_mappings (process_name, task_name, url, screen, first_header_title, second_header_title_name, input_variables, output_variables) values ('#{value[0]}', '#{value[1]}', '#{value[2]}', '#{value[3]}', '#{value[4]}', '#{value[5]}', '#{value[6]}', '#{value[7]}')")
     end
@@ -356,5 +356,21 @@ class InsertInitialValues < ActiveRecord::Migration
     ].each do |value|
       execute("insert into mst_spt_spare_part_status_action (code, name, manufacture_type_index, store_nc_type_index, store_ch_type_index, on_loan_type_index) values ('#{value[0]}', '#{value[1]}', '#{value[2]}', '#{value[3]}', '#{value[4]}', '#{value[5]}')")
     end
+
+    [
+      ["ML", "Email"],
+      ["SM", "SMS"],
+      ["CL", "Call"],
+      ["FX", "Fax"]
+    ].each do |value|
+      execute("insert into mst_spt_contact_type (code, name) values ('#{value[0]}', '#{value[1]}')")
+    end
+
+    [
+      [nil, "PRINT_SPPT_INVOICE", "PRINT_SPPT_TICKET", "PRINT_SPPT_TICKET_COMPLETE", "PRINT_SPPT_FSR"]
+    ].each do |value|
+      execute("insert into mst_spt_templates (invoice, invoice_request_type, ticket_request_type, ticket_complete_request_type, fsr_request_type) values ('#{value[0]}', '#{value[1]}', '#{value[2]}', '#{value[3]}', '#{value[4]}')")
+    end
+
   end
 end
