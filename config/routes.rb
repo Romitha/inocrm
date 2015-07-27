ed0fb21c07593f5cc1c765e5092e58e11480eadd
sharmilan <sharmilan.somasundaram@inovaitsys.com>
2015-07-27 14:40:10 +0530

    admin dashboard master data front-end is updated.

--- config/routes.rb
+++ config/routes.rb
@@ -156,6 +156,7 @@ Rails.application.routes.draw do
       get "open-tickets", :action => :open_tickets
       get "closed-tickets", :action => :closed_tickets
       get "total-products", :action => :total_products
+      get "reason"
     end
   end
      
