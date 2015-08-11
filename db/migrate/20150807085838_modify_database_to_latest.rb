2a38d1f09a992415b106d6d21dc2059e1aca6086
ranuri <ranuri.lakma@inovaitsys.com>
2015-08-11 10:13:41 +0530

    ticket header template and db changes.

--- db/migrate/20150807085838_modify_database_to_latest.rb
+++ db/migrate/20150807085838_modify_database_to_latest.rb
@@ -1,27 +1,27 @@
 class ModifyDatabaseToLatest < ActiveRecord::Migration
   def change
-    # remove_foreign_key :mst_inv_product, column: :created_by
+    remove_foreign_key :mst_inv_product, column: :created_by
 
-    # add_index :mst_spt_spare_part_description, :description, unique: true
+    add_index :mst_spt_spare_part_description, :description, unique: true
 
-    # change_column_null :spt_act_hold, :un_hold_action_id, true
+    change_column_null :spt_act_hold, :un_hold_action_id, true
 
-    # remove_foreign_key :spt_act_request_spare_part, column: :terminate_reason_id
+    remove_foreign_key :spt_act_request_spare_part, column: :terminate_reason_id
 
-    # rename_column :spt_act_request_spare_part, :terminate_reason_id, :part_terminate_reason_id
-    # add_foreign_key(:spt_act_request_spare_part, :mst_spt_reason, name: "fk_spt_act_request_spare_part_mst_spt_reason1", column: :part_terminate_reason_id)
+    rename_column :spt_act_request_spare_part, :terminate_reason_id, :part_terminate_reason_id
+    add_foreign_key(:spt_act_request_spare_part, :mst_spt_reason, name: "fk_spt_act_request_spare_part_mst_spt_reason1", column: :part_terminate_reason_id)
 
-    # rename_column :spt_ticket, :terminated, :ticket_terminated
+    rename_column :spt_ticket, :terminated, :ticket_terminated
 
-    # change_column_null :spt_ticket_on_loan_spare_part, :ref_spare_part_id, true
+    change_column_null :spt_ticket_on_loan_spare_part, :ref_spare_part_id, true
 
-    # rename_column :spt_ticket_on_loan_spare_part, :terminated, :part_terminated
-    # remove_foreign_key :spt_ticket_on_loan_spare_part, column: :terminated_reason_id
-    # rename_column :spt_ticket_on_loan_spare_part, :terminated_reason_id, :part_terminated_reason_id
+    rename_column :spt_ticket_on_loan_spare_part, :terminated, :part_terminated
+    remove_foreign_key :spt_ticket_on_loan_spare_part, column: :terminated_reason_id
+    rename_column :spt_ticket_on_loan_spare_part, :terminated_reason_id, :part_terminated_reason_id
 
-    # add_foreign_key(:spt_ticket_on_loan_spare_part, :mst_spt_reason, name: "fk_spt_ticket_on_loan_spare_part_mst_spt_reason1", column: :part_terminated_reason_id)
+    add_foreign_key(:spt_ticket_on_loan_spare_part, :mst_spt_reason, name: "fk_spt_ticket_on_loan_spare_part_mst_spt_reason1", column: :part_terminated_reason_id)
 
-    # rename_column :spt_ticket_spare_part, :terminated, :part_terminated
+    rename_column :spt_ticket_spare_part, :terminated, :part_terminated
     remove_foreign_key :spt_ticket_spare_part, column: :terminated_reason_id
     rename_column :spt_ticket_spare_part, :terminated_reason_id, :part_terminated_reason_id
 
