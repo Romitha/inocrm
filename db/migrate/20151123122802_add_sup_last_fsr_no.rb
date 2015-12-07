9154b0fba6230941f4990fbd95b0c82246c734b4
ranuri <ranuri.lakma@inovaitsys.com>
2015-12-07 14:06:04 +0530

    db change add_sup_last_fsr_no

--- db/migrate/20151123122802_add_sup_last_fsr_no.rb
+++ db/migrate/20151123122802_add_sup_last_fsr_no.rb
@@ -11,11 +11,19 @@ class AddSupLastFsrNo < ActiveRecord::Migration
     change_column :spt_ticket, :ticket_no, "INT(11) UNSIGNED NOT NULL"
     change_column :inv_grn, :grn_no, "INT UNSIGNED NOT NULL"
     change_column :inv_prn, :prn_no, "INT UNSIGNED NOT NULL"
+    change_column :inv_prn, :prn_no, "INT UNSIGNED NOT NULL"
     change_column :inv_sbn, :sbn_no, "INT UNSIGNED NOT NULL"
     change_column :inv_srn, :srn_no, "INT UNSIGNED NOT NULL"
     change_column :inv_srr, :srr_no, "INT UNSIGNED NOT NULL"
     change_column :mst_inv_product_info, :need_serial, :boolean
     change_column :inv_inventory_serial_item, :batch_id, "INT UNSIGNED NULL DEFAULT NULL"
+
+    #______________________________test_____________________________________________________
+    # change_column :inv_inventory_serial_item, :batch_id, :integer, default: nil
+    # change_column :inv_inventory_serial_item, :batch_id, "INT DEFAULT NULL"
+    # change_column :inv_inventory_serial_item, :batch_id, "INT UNSIGNED NULL DEFAULT NULL"
+    # change_column :inv_inventory_serial_item, :batch_id, :integer, :default => nil, :null => true
+    #_______________________________________________________________________________________________
 
     add_column :inv_gin, :gin_no, "INT UNSIGNED NOT NULL"
     add_column :mst_inv_product_info, :need_batch, :boolean
@@ -29,17 +37,41 @@ class AddSupLastFsrNo < ActiveRecord::Migration
     change_column :inv_inventory_serial_item, :id, "INT UNSIGNED NOT NULL AUTO_INCREMENT"
     execute "ALTER TABLE inv_inventory_serial_item ADD PRIMARY KEY(id);"
 
+
     add_foreign_key :spt_product_serial, :inv_inventory_serial_item, name: :fk_spt_product_serial_inv_inventory_serial_item1, column: :inventory_serial_item_id
     add_foreign_key :inv_grn_serial_item, :inv_inventory_serial_item, name: :fk_grn_serial_inventory_serial_item1, column: :serial_item_id
     add_foreign_key :inv_inventory_serial_part, :inv_inventory_serial_item, name: :fk_inv_inventory_serial_part_inv_inventory_serial_item1, column: :serial_item_id
     add_foreign_key :inv_serial_additional_cost, :inv_inventory_serial_item, name: :fk_inv_serial_warranty_inv_inventory_serial_item10, column: :serial_item_id
     add_foreign_key :inv_serial_warranty, :inv_inventory_serial_item, name: :fk_inv_serial_warranty_inv_inventory_serial_item1, column: :serial_item_id
 
-
-    add_column :inv_srn_item, :main_product_id, "INT UNSIGNED NULL DEFAULT NULL AFTER spare_part"
+    add_column :inv_srn_item, :main_product_id, "INT UNSIGNED NULL DEFAULT NULL"
     add_index :inv_srn_item, :main_product_id
     add_foreign_key :inv_srn_item, :mst_inv_product, name: :fk_srn_item_main_mst_product1, column: :main_product_id
 
+    add_column :inv_gin_item, :main_product_id, "INT UNSIGNED NULL DEFAULT NULL"
+    add_index :inv_gin_item, :main_product_id
+    add_foreign_key :inv_gin_item, :mst_inv_product, name: :fk_gin_item_main_mst_product, column: :main_product_id
+
+    add_column :spt_ticket_spare_part_store, :inv_gin_id, "INT UNSIGNED NULL DEFAULT NULL"
+    add_column :spt_ticket_spare_part_store, :inv_gin_item_id, "INT UNSIGNED NULL DEFAULT NULL"
+    add_index :spt_ticket_spare_part_store, :inv_gin_item_id
+    add_foreign_key :spt_ticket_spare_part_store, :inv_gin, name: :fk_spt_ticket_spare_part_inv_gin_id, column: :inv_gin_id
+
+    add_column :spt_ticket_on_loan_spare_part, :inv_gin_id, "INT UNSIGNED NULL DEFAULT NULL"
+    add_column :spt_ticket_on_loan_spare_part, :inv_gin_item_id, "INT UNSIGNED NULL DEFAULT NULL"
+    add_index :spt_ticket_on_loan_spare_part, :inv_gin_id
+    add_index :spt_ticket_on_loan_spare_part, :inv_gin_item_id
+    add_foreign_key :spt_ticket_on_loan_spare_part, :inv_gin, name: :fk_spt_ticket_on_loan_spare_part_inv_gin_id, column: :inv_gin_id
+    add_foreign_key :spt_ticket_on_loan_spare_part, :inv_gin_item, name: :fk_spt_ticket_on_loan_spare_part_inv_gin_item_id, column: :inv_gin_item_id
+
+    add_column :inv_gin_item, :inventory_not_updated, :boolean
+    add_column :inv_grn_item, :inventory_not_updated, :boolean
+    add_column :inv_grn_item, :main_product_id, "INT UNSIGNED NULL DEFAULT NULL"
+    add_index :inv_grn_item, :main_product_id
+    add_foreign_key :inv_grn_item, :mst_inv_product, name: :fk_grn_item_main_mst_product1, column: :main_product_id
+    add_column :inv_grn_serial_item, :inv_serial_part_id, "INT UNSIGNED NULL DEFAULT NULL"
+    add_index :inv_grn_serial_item, :inv_serial_part_id
+    add_foreign_key :inv_grn_serial_item, :inv_inventory_serial_part, name: :fk_grn_serial_inventory_serial_part, column: :inv_serial_part_id
 
   end
 end
@@ -55,7 +87,6 @@ end
 # ALTER TABLE `mst_inv_product_info` ADD `need_batch` TINYINT NOT NULL DEFAULT '0' AFTER `need_serial`;
 # ALTER TABLE `mst_inv_product_info` CHANGE `need_serial` `need_serial` TINYINT(1) NOT NULL DEFAULT '0';
 # ALTER TABLE `inv_inventory_serial_item` CHANGE `batch_id` `batch_id` INT(10) UNSIGNED NULL DEFAULT NULL;
-
 # ALTER TABLE `inv_srn_item` ADD `main_product_id` INT UNSIGNED NULL DEFAULT NULL AFTER `spare_part`;
 # ALTER TABLE `inv_srn_item` ADD INDEX( `main_product_id`);
 # ALTER TABLE `inv_srn_item` ADD CONSTRAINT `fk_srn_item_main_mst_product1` FOREIGN KEY (`main_product_id`) REFERENCES `inocrm_dev`.`mst_inv_product`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
