addd4b114c3214bcf8bfad5ebb05fda6aa0aeb87
Umesh M <umeshblader@gmail.com>
2016-04-20 14:15:19 +0530

    database table errors are corrected and updated with latest update

--- db/migrate/20160325091700_add_spt_ticket_invoice_terminate.rb
+++ db/migrate/20160325091700_add_spt_ticket_invoice_terminate.rb
@@ -12,7 +12,7 @@ class AddSptTicketInvoiceTerminate < ActiveRecord::Migration
   	add_index :spt_ticket_invoice_terminate, :terminate_job_payment_id, name: "fk_spt_ticket_invoice_terminate_spt_act_terminate_job_payme_idx"
   	add_index :spt_ticket_invoice_terminate, :invoice_id, name: "fk_spt_ticket_invoice_terminate_spt_ticket_invoice1_idx"
 
-    add_foreign_key :spt_ticket_invoice_terminate, :spt_act_terminate_job_payment, column: :terminate_job_payment_id, name: :fk_spt_ticket_invoice_terminate_spt_act_terminate_job_payment1, primary_key: :ticket_action_id
+    add_foreign_key :spt_ticket_invoice_terminate, :spt_act_terminate_job_payment, column: :terminate_job_payment_id, name: :fk_spt_ticket_invoice_terminate_spt_act_terminate_job_payment1#, primary_key: :ticket_action_id
     add_foreign_key :spt_ticket_invoice_terminate, :spt_ticket_invoice, column: :invoice_id, name: :fk_spt_ticket_invoice_terminate_spt_ticket_invoice1
 
   end
