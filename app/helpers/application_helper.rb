a4a51426f3007e19b3ef6ecc3e32d45b1a73105f
Umesh M <umeshblader@gmail.com>
2016-06-07 20:44:28 +0530

    print invoice spare_part_store is modified to ticket_spare_part_store

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -286,7 +286,7 @@ module ApplicationHelper
         estimation_part = ticket_invoice_estimation.ticket_estimation.ticket_estimation_parts.first
         item_index += 1
         description = "Part No: #{estimation_part.ticket_spare_part.spare_part_no} #{estimation_part.ticket_spare_part.spare_part_description}"
-        item_code = estimation_part.ticket_spare_part.spare_part_store.present? ? estimation_part.ticket_spare_part.spare_part_store.approved_inventory_product.item_code : ""
+        item_code = estimation_part.ticket_spare_part.ticket_spare_part_store.present? ? estimation_part.ticket_spare_part.ticket_spare_part_store.approved_inventory_product.item_code : ""
 
         unit_price = ticket_invoice_estimation.ticket_estimation.approval_required ? estimation_part.approved_estimated_price : estimation_part.estimated_price
 
