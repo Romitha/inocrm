6b987e5a6f5dd29b8ef2ff3f30ae8222b0099f58
Umesh M <umeshblader@gmail.com>
2016-06-22 23:58:15 +0530

    mst_contact_type_validate table is corrected.

--- db/migrate/20160622073749_update_db_customer_contact.rb
+++ db/migrate/20160622073749_update_db_customer_contact.rb
@@ -1,7 +1,8 @@
 class UpdateDbCustomerContact < ActiveRecord::Migration
   def change
-    create_table :mst_spt_contact_type_validate, id: false do |t|
+    create_table :mst_contact_type_validate, id: false do |t|
       t.column :id, "INT UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
+      t.string :code, null: false
     end
 
     add_column :mst_spt_customer_contact_type, :validate_id, "INT UNSIGNED NOT NULL"
@@ -9,7 +10,7 @@ class UpdateDbCustomerContact < ActiveRecord::Migration
 
     execute "SET FOREIGN_KEY_CHECKS = 0"
 
-    add_foreign_key :mst_spt_customer_contact_type, :mst_spt_contact_type_validate, name: "fk_mst_spt_customer_contact_type_mst_spt_contact_type_validate1", column: :validate_id
+    add_foreign_key :mst_spt_customer_contact_type, :mst_contact_type_validate, name: "fk_mst_spt_customer_contact_type_mst_contact_type_validate1", column: :validate_id
 
     add_column :mst_spt_templates, :ticket_sticker_request_type, :string, default: "PRINT_SPPT_TICKET_STICKER"
     add_column :mst_spt_templates, :ticket_sticker, :text
