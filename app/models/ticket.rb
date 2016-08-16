27a996671cceaf2d17a898d472f3e2f7cb379a62
Umesh M <umeshblader@gmail.com>
2016-08-16 18:11:52 +0530

    ticket elasticsearch bug is corrected.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -28,7 +28,7 @@ class Ticket < ActiveRecord::Base
   def to_indexed_json
     Warranty
     to_json(
-      only: [:created_at, :cus_chargeable, :id],
+      only: [:created_at, :cus_chargeable, :id, :created_at],
       methods: [:customer_name, :ticket_status_name, :warranty_type_name, :support_ticket_no],
       include: {
         products: {
