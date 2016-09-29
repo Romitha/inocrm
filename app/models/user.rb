0782b375f164d81f8a785c18aa9afb844484558a
Umesh M <umeshblader@gmail.com>
2016-09-29 19:06:00 +0530

    rolify helper methods are removed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 
   mount_uploader :avatar, AvatarUploader
 
-  rolify
+  # rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
