32f82e6fc2b13aec45f9ca906d6d0f99e945eb16
Umesh M <umeshblader@gmail.com>
2016-09-29 19:06:00 +0530

    rolify helper methods are removed.

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,7 +3,7 @@ class Role < ActiveRecord::Base
   belongs_to :resource, :polymorphic => true
   
   has_and_belongs_to_many :rpermissions
-  scopify
+  # scopify
 
   validates :name, presence: true, uniqueness: true
 
