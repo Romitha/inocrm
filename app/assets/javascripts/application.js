ce2cae10b576225be047ea62156d0bccb41dee07
Umesh M <umeshblader@gmail.com>
2016-10-18 18:20:26 +0530

    phoenix installed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,7 +30,7 @@
 //= require bootstrap-wysihtml5
 //= require chosen-jquery
 //= require mustache
-//= require sisyphus.min
+//= require phoenix
 //= require_tree .
 $.fn.editable.defaults.ajaxOptions = {type: "PUT", mode: "inline"};
 $.fn.editable.defaults.error = function(response, newValue){
