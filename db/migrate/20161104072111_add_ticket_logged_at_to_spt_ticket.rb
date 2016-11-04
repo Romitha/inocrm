ca3cb438f1630d6d4a4eed7bf981aeda34b5aab8
Umesh M <umeshblader@gmail.com>
2016-11-04 16:59:09 +0530

    database updated.

--- db/migrate/20161104072111_add_ticket_logged_at_to_spt_ticket.rb
+++ db/migrate/20161104072111_add_ticket_logged_at_to_spt_ticket.rb
@@ -1,10 +1,10 @@
 class AddTicketLoggedAtToSptTicket < ActiveRecord::Migration
   def change
     # execute "ALTER TABLE spt_ticket ADD logged_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP"
-    # add_column :spt_ticket, :logged_by, :integer, null: false
-    # add_index :spt_ticket, :logged_by, name: "spt_ticket_logged_by_user_index_idx"
-    # add_column :spt_ticket, :updated_by, :integer
-    # add_index :spt_ticket, :updated_by, name: "spt_ticket_updated_by_user_index_idx"
+    add_column :spt_ticket, :logged_by, "INT(10) UNSIGNED NOT NULL"
+    add_index :spt_ticket, :logged_by, name: "spt_ticket_logged_by_user_index_idx"
+    add_column :spt_ticket, :updated_by, "INT(10) UNSIGNED"
+    add_index :spt_ticket, :updated_by, name: "spt_ticket_updated_by_user_index_idx"
 
     execute "SET FOREIGN_KEY_CHECKS = 0"
     add_foreign_key :spt_ticket, :users, name: "spt_ticket_logged_by_user_foreign_id", column: :logged_by
