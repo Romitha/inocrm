37a82fa8226f27af6de10dbf1186468af41bf122
Umesh M <umeshblader@gmail.com>
2016-11-04 17:01:47 +0530

    last database migration is updated.

--- db/migrate/20161104072111_add_ticket_logged_at_to_spt_ticket.rb
+++ db/migrate/20161104072111_add_ticket_logged_at_to_spt_ticket.rb
@@ -1,6 +1,6 @@
 class AddTicketLoggedAtToSptTicket < ActiveRecord::Migration
   def change
-    # execute "ALTER TABLE spt_ticket ADD logged_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP"
+    execute "ALTER TABLE spt_ticket ADD logged_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP"
     add_column :spt_ticket, :logged_by, "INT(10) UNSIGNED NOT NULL"
     add_index :spt_ticket, :logged_by, name: "spt_ticket_logged_by_user_index_idx"
     add_column :spt_ticket, :updated_by, "INT(10) UNSIGNED"
