a8f2141776940d6b72f6ecf7243b605a224e2015
Umesh M <umesh.m@inovaitsys.com>
2016-12-20 12:32:15 +0530

    issue part backend is corrected.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2798,7 +2798,6 @@ class TicketsController < ApplicationController
         # grn_items = @onloan_or_store.approved_inventory_product.grn_items.search(query: "grn.store_id:#{@onloan_or_store.approved_store_id} AND inventory_not_updated:false AND remaining_quantity:>0")
         grn_items = GrnItem.search(query: "grn.store_id:#{@onloan_or_store.approved_store_id} AND inventory_not_updated:false AND remaining_quantity:>0 AND inventory_product.id:#{@onloan_or_store.approved_inventory_product.id}")
 
-
         @grns = Kaminari.paginate_array(grn_items).page(params[:page]).per(10)
       end
     end
