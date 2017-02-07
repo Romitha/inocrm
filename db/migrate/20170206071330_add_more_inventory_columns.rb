4065cafe5039fccefea898da895b42ff34c28821
malshan <malshan.gunawardana@inovaitsys.com>
2017-02-07 14:54:05 +0530

    approve store parts bugs and db migration

--- db/migrate/20170206071330_add_more_inventory_columns.rb
+++ db/migrate/20170206071330_add_more_inventory_columns.rb
@@ -1,6 +1,6 @@
 class AddMoreInventoryColumns < ActiveRecord::Migration
   def change
-    add_column :fk_inv_srr_item_mst_currency1_idxtem, :total_cost, "DECIMAL(13,2) NOT NULL DEFAULT 0"
+    add_column :inv_srr_item, :total_cost, "DECIMAL(13,2) NOT NULL DEFAULT 0"
     add_column :inv_srr_item, :currency_id, "INT UNSIGNED NOT NULL"
     add_index :inv_srr_item, :currency_id, name: "fk_inv_srr_item_mst_currency1_idx"
     add_foreign_key :inv_srr_item, :currency_id
