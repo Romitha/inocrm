e3ad9c995f58819ed224ef4fa78e84a63164b1da
Umesh M <umesh.m@inovaitsys.com>
2017-06-06 19:08:42 +0530

    migration updated with latest update

--- db/migrate/20170602095234_rpermission_column_update.rb
+++ db/migrate/20170602095234_rpermission_column_update.rb
@@ -1,15 +1,15 @@
 class RpermissionColumnUpdate < ActiveRecord::Migration
   def change
 
-    remove_foreign_key :rpermissions, name: :fk_main_permissions_main_permissions2
-    remove_index :rpermissions, name: :fk_main_permissions_main_permissions2_idx
-    remove_column :rpermissions, :subject_attribute_id
+    # remove_foreign_key :rpermissions, name: :fk_main_permissions_main_permissions2
+    # remove_index :rpermissions, name: :fk_main_permissions_main_permissions2_idx
+    # remove_column :rpermissions, :subject_attribute_id
 
-    remove_foreign_key :rpermissions, name: :fk_main_permissions_main_permissions1
-    remove_index :rpermissions, name: :fk_main_permissions_main_permissions1_idx
-    remove_column :rpermissions, :subject_action_id
+    # remove_foreign_key :rpermissions, name: :fk_main_permissions_main_permissions1
+    # remove_index :rpermissions, name: :fk_main_permissions_main_permissions1_idx
+    # remove_column :rpermissions, :subject_action_id
 
-    remove_column :spt_ticket, :updated_by
+    # remove_column :spt_ticket, :updated_by
 
     add_column :subject_actions, :rpermission_id, "INT UNSIGNED NOT NULL"
     add_index :subject_actions, :rpermission_id, name: "fk_subject_actions_rpermission_id1_idx"
