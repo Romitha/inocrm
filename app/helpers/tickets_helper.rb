d006d80b34b2f1c9b1fd91f38f7d5c02c46e11b4
malshan <malshan.gunawardana@inovaitsys.com>
2017-07-20 11:54:52 +0530

    workflow - Engineers time allocation

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,17 @@
 module TicketsHelper
+  def convert_hours_minutes(value)
+    result_array = []
+    a, b = value.to_f.divmod(3600)
+    result_array << "Hours: #{a}"
+
+    if b/60 > 1
+      result_array << "Minutes: #{b.to_f.divmod(60).first}"
+      result_array << "Seconds: #{b.to_i.divmod(60).last}"
+    else
+      result_array << "Seconds: #{b.to_i}"
+    end
+
+    result_array.join(" ")
+
+  end
 end
