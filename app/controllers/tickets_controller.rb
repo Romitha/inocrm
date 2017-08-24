1918c84a5c1135d7ddd768f80364f75734434ad8
umesh1 <umesh1@inovaitsys.com>
2017-08-24 15:55:04 +0530

    Ticket workflow diagram and null error is updated

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3719,8 +3719,7 @@ class TicketsController < ApplicationController
 
     @workflow_process_ids = @workflow_processes.map { |p| p.process_id }
 
-    @workflow_process_ids << ticket_engineer.ticket_workflow_processes.pluck(:process_id) if ticket_engineer.present?
-
+    @workflow_process_ids << ticket_engineer.ticket_workflow_processes.where(ticket_id: @ticket.id).pluck(:process_id) if ticket_engineer.present?
     @task_list = []
     @workflow_process_ids.each do |workflow_process_id|
 
