4047dac1b8b8ffd9c79dcab234d1e18152ea4959
Umesh M <umesh.m@inovaitsys.com>
2017-08-29 12:24:28 +0530

    Elasticsearch order mechanism updated in GRN Item

--- app/models/grn.rb
+++ app/models/grn.rb
@@ -300,7 +300,7 @@ class GrnItem < ActiveRecord::Base
           # puts params[:store_id]
         end
       end
-      sort { by :created_at, "desc" }
+      sort { by :created_at, {order: "desc", ignore_unmapped: true} }
       # filter :range, published_at: { lte: Time.zone.now}
       # raise to_curl
     end
