5a89a4ccc1f70ff66049e46c6e09e6abf7b605b5
umesh1 <umesh1@inovaitsys.com>
2017-09-28 19:02:23 +0530

    migration file updated

--- db/migrate/20161006132513_modify_roles_related_tables.rb
+++ db/migrate/20161006132513_modify_roles_related_tables.rb
@@ -1,57 +1,57 @@
 class ModifyRolesRelatedTables < ActiveRecord::Migration
   def change
-    remove_column :rpermissions, :controller_resource, :controller_action
+    # remove_column :rpermissions, :controller_resource, :controller_action
 
-    add_column :roles_rpermissions, :subject_value, :string
+    # add_column :roles_rpermissions, :subject_value, :string
 
-    create_table :subject_classes, id: false do |t|
-      t.column :id, "INT UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
-      t.string :name
-      t.column :subject_base_id, "INT UNSIGNED"
+    # create_table :subject_classes, id: false do |t|
+    #   t.column :id, "INT UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
+    #   t.string :name
+    #   t.column :subject_base_id, "INT UNSIGNED"
 
-      t.timestamps
+    #   t.timestamps
 
-      t.index :subject_base_id, name: "fk_system_resources_resource_bases1_idx"
-    end
+    #   t.index :subject_base_id, name: "fk_system_resources_resource_bases1_idx"
+    # end
 
-    create_table :subject_attributes, id: false do |t|
-      t.column :id, "INT UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
-      t.string :name
-      t.string :value
-      t.column :rpermission_id, "INT UNSIGNED"
-      t.timestamps
-    end
+    # create_table :subject_attributes, id: false do |t|
+    #   t.column :id, "INT UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
+    #   t.string :name
+    #   t.string :value
+    #   t.column :rpermission_id, "INT UNSIGNED"
+    #   t.timestamps
+    # end
 
-    create_table :subject_actions, id: false do |t|
-      t.column :id, "INT UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
-      t.string :name
-      t.column :subject_action_alias_id, "INT UNSIGNED"
-      t.column :rpermission_id, "INT UNSIGNED"
-      t.timestamps
+    # create_table :subject_actions, id: false do |t|
+    #   t.column :id, "INT UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
+    #   t.string :name
+    #   t.column :subject_action_alias_id, "INT UNSIGNED"
+    #   t.column :rpermission_id, "INT UNSIGNED"
+    #   t.timestamps
 
-      t.index :subject_action_alias_id, name: "fk_subject_actions_subject_actions1_idx"
-    end
+    #   t.index :subject_action_alias_id, name: "fk_subject_actions_subject_actions1_idx"
+    # end
 
-    create_table :subject_bases, id: false do |t|
-      t.column :id, "INT UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
-      t.string :name
-      t.timestamps
-    end
+    # create_table :subject_bases, id: false do |t|
+    #   t.column :id, "INT UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
+    #   t.string :name
+    #   t.timestamps
+    # end
 
 
-    # add_column :rpermissions, :subject_class_id, "INT UNSIGNED NOT NULL"
-    # add_column :rpermissions, :subject_attribute_id, "INT UNSIGNED NOT NULL"
-    # add_column :rpermissions, :subject_action_id, "INT UNSIGNED NOT NULL"
+    # # add_column :rpermissions, :subject_class_id, "INT UNSIGNED NOT NULL"
+    # # add_column :rpermissions, :subject_attribute_id, "INT UNSIGNED NOT NULL"
+    # # add_column :rpermissions, :subject_action_id, "INT UNSIGNED NOT NULL"
 
-    add_index :rpermissions, :subject_class_id, name: "fk_main_permissions_subject_classes1_idx"
-    add_index :subject_attributes, :rpermission_id, name: "fk_main_permissions_main_permissions1_idx"
-    add_index :subject_actions, :rpermission_id, name: "fk_subject_actions_main_permissions1_idx"
+    # add_index :rpermissions, :subject_class_id, name: "fk_main_permissions_subject_classes1_idx"
+    # add_index :subject_attributes, :rpermission_id, name: "fk_main_permissions_main_permissions1_idx"
+    # add_index :subject_actions, :rpermission_id, name: "fk_subject_actions_main_permissions1_idx"
 
-    add_foreign_key(:subject_classes, :subject_bases, name: "fk_system_resources_resource_bases1", column: :subject_base_id)
-    add_foreign_key(:subject_actions, :subject_actions, name: "fk_system_resources_resource_actions1", column: :subject_action_alias_id)
+    # add_foreign_key(:subject_classes, :subject_bases, name: "fk_system_resources_resource_bases1", column: :subject_base_id)
+    # add_foreign_key(:subject_actions, :subject_actions, name: "fk_system_resources_resource_actions1", column: :subject_action_alias_id)
 
-    execute "SET FOREIGN_KEY_CHECKS = 0"
-    add_foreign_key(:rpermissions, :subject_classes, name: "fk_main_permissions_subject_classes1", column: :subject_class_id)
+    # execute "SET FOREIGN_KEY_CHECKS = 0"
+    # add_foreign_key(:rpermissions, :subject_classes, name: "fk_main_permissions_subject_classes1", column: :subject_class_id)
     add_foreign_key(:subject_attributes, :rpermissions, name: "fk_main_permissions_main_permissions2", column: :rpermission_id)
     add_foreign_key(:subject_actions, :rpermissions, name: "fk_main_permissions_main_permissions1", column: :rpermission_id)
     execute "SET FOREIGN_KEY_CHECKS = 1"
