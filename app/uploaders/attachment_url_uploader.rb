7166ba457d90af13cf5d262ef336988eef01f32f
umesh1 <umesh1@inovaitsys.com>
2017-11-10 19:28:14 +0530

    contract initiated.

--- app/uploaders/attachment_url_uploader.rb
+++ app/uploaders/attachment_url_uploader.rb
@@ -4,11 +4,19 @@ class AttachmentUrlUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
+
+  if Rails.env == "development"
+    storage :file
+
+  else
+    storage :sftp
+
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
