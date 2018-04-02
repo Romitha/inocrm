0a94ccbaeb7a5d35be555af4a194602311d4a6df
umesh1 <umesh1@inovaitsys.com>
2018-04-02 16:35:37 +0530

    testing12

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -915,7 +915,7 @@ class TicketsController < ApplicationController
         if e.is_a?(ActiveRecord::RecordNotUnique)
           render js: "alert('Ticket token already taken. Please try again saving. It will save with fresh ticket token');"
         else
-          render js: "alert('Some error occured. Please try again'); "
+          render js: "alert('Some error occured. Please try again');"
         end
       end
       
