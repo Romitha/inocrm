bd95b7e720fd327cc893e82028a46cf89465398d
umesh1 <umesh1@inovaitsys.com>
2018-04-02 17:20:36 +0530

    testing17

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -414,9 +414,9 @@ class CompanyConfig < ActiveRecord::Base
         true
       end
 
-      inspectable = {last_ticket_no: last_ticket_no, started_last_ticket_no: started_last_ticket_no, looped: looped_count, ticket_no: ticket.ticket_no, sup_last_ticket_no: self.sup_last_ticket_no, started_time: started_time.to_s, finished_time: finished_time.to_s}
+      # inspectable = {last_ticket_no: last_ticket_no, started_last_ticket_no: started_last_ticket_no, looped: looped_count, ticket_no: ticket.ticket_no, sup_last_ticket_no: self.sup_last_ticket_no, started_time: started_time.to_s, finished_time: finished_time.to_s}
 
-      File.open(Rails.root.join("bug_file.txt"), "a"){|file| file.write("inspectable: #{inspectable}\n"); file.close}
+      # File.open(Rails.root.join("bug_file.txt"), "a"){|file| file.write("inspectable: #{inspectable}\n"); file.close}
 
       true
     end
