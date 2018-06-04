965cc46aff2aef46d17c5f5c9b79898c42acea65
Farah Jabir <f.farahjabir@gmail.com>
2018-06-04 13:46:42 +0530

    updates migration is deleted.

--- db/migrate/20180525072138_workflow_mapping_updates.rb
+++
@@ -1,7 +0,0 @@
-class WorkflowMappingUpdates < ActiveRecord::Migration
-  def change
-    add_column :workflow_mappings, :bpm_role_id, "INT UNSIGNED NULL"
-    add_index :workflow_mappings, :bpm_role_id, name: "fk_workflow_mappings_mst_bpm_role1_idx"
-    add_foreign_key :workflow_mappings, :mst_bpm_role, name: :fk_workflow_mappings_mst_bpm_role1, column: :bpm_role_id
-  end
-end
