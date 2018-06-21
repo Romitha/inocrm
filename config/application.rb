2e741893bc09af3174bc4f94e49fe59f707efe08
umesh1 <umesh1@inovaitsys.com>
2018-06-21 16:29:39 +0530

    configuration is updated.

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module InovaCrm
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     # config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
+    config.web_console.development_only = false
   end
 end
