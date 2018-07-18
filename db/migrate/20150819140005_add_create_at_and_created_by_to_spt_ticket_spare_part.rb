58d0b50db9acc9009b473de36d44a2deb3c2ebef
Farah Jabir <f.farahjabir@gmail.com>
2018-07-18 15:45:12 +0530

    bug is corrected.

--- db/migrate/20150819140005_add_create_at_and_created_by_to_spt_ticket_spare_part.rb
+++ db/migrate/20150819140005_add_create_at_and_created_by_to_spt_ticket_spare_part.rb
@@ -3,8 +3,8 @@ class AddCreateAtAndCreatedByToSptTicketSparePart < ActiveRecord::Migration
     add_column :spt_ticket_spare_part, :requested_at, :datetime
     add_column :spt_ticket_spare_part, :requested_by, :integer
 
-    # add_index :spt_ticket_spare_part, :requested_by, unique: true
+    add_index :spt_ticket_spare_part, :requested_by #, unique: true
 
-    # add_foreign_key :spt_ticket_spare_part, :users, name: "fk_spt_act_request_spare_part_users", column: :requested_by
+    add_foreign_key :spt_ticket_spare_part, :users, name: "fk_spt_act_request_spare_part_users", column: :requested_by
   end
 end
