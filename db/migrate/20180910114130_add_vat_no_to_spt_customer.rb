a2d4db6cffd50ea97079c820594cce75ad9647c8
umesh1 <umesh1@inovaitsys.com>
2018-09-12 12:17:20 +0530

    spt_customers is corrected to spt_customer

--- db/migrate/20180910114130_add_vat_no_to_spt_customer.rb
+++ db/migrate/20180910114130_add_vat_no_to_spt_customer.rb
@@ -1,6 +1,6 @@
 class AddVatNoToSptCustomer < ActiveRecord::Migration
   def change
-    add_column :spt_customers, :vat_no, :string, limit: 100  # "VARCHAR(100) NULL"
-    add_column :spt_customers, :svat_no, :string, limit: 100 # "VARCHAR(100) NULL"
+    add_column :spt_customer, :vat_no, :string, limit: 100  # "VARCHAR(100) NULL"
+    add_column :spt_customer, :svat_no, :string, limit: 100 # "VARCHAR(100) NULL"
   end
 end
