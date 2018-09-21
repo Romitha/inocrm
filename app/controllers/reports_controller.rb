7606f121eedabce8fe649d42a3c44fc81f0b9e15
Farah Jabir <f.farahjabir@gmail.com>
2018-09-21 14:47:34 +0530

    new update

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -33,7 +33,7 @@ class ReportsController < ApplicationController
     address4 = ticket.customer.address4
 
     cus_contact_person = ticket.send("contact_person#{ticket.inform_cp}").full_name
-
+    
     cus_contact_info = ticket.send("contact_person#{ticket.inform_cp}").contact_person_contact_types.map(&:contact_info)
 
     vat_no  = ticket.customer.vat_no
